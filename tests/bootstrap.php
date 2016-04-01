2238cc14b17a2d961cc2dbaa5a04c7958aee51c5
Christoph Kappestein <k42b3.x@gmail.com>
2016-04-01 21:10:00 +0200

    update bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,12 +3,6 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 \Doctrine\Common\Annotations\AnnotationRegistry::registerLoader(function ($class) {
-
-    if (strpos($class, 'PSX\\Schema\\Parser\\Popo\\Annotation') === 0) {
-        spl_autoload_call($class);
-
-        return class_exists($class, false);
-    }
-
+    spl_autoload_call($class);
+    return class_exists($class, false);
 });
-
