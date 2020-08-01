79a62d544a6bfacfa65fdfee5dc220d10dafc0a2
Christoph Kappestein <christoph.kappestein@gmail.com>
2020-08-01 22:23:55 +0200

    update schema namespace

--- tests/ProcessorTestCase.php
+++ tests/ProcessorTestCase.php
@@ -43,7 +43,7 @@ abstract class ProcessorTestCase extends TestCase
     protected function setUp(): void
     {
         $reader = new SimpleAnnotationReader();
-        $reader->addNamespace('PSX\\Schema\\Parser\\Popo\\Annotation');
+        $reader->addNamespace('PSX\\Schema\\Annotation');
 
         $processor = new Processor(Configuration::createDefault($reader, new SchemaManager($reader)));
 
